1a6dbb8834d4b29ad9170ec0314046166668f066
Stevecv <37953488+Stevecv@users.noreply.github.com>
2024-12-12 16:19:51 +0000

    Working

--- Day4/Part2.java
+++ Day4/Part2.java
@@ -1,18 +1,8 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Day4B {
-    public static String code = "MMMSXXMASM\n" +
-            "MSAMXMSMSA\n" +
-            "AMXSXMAAMM\n" +
-            "MSAMASMSMX\n" +
-            "XMASAMXAMM\n" +
-            "XXAMMXXAMA\n" +
-            "SMSMSASXSS\n" +
-            "SAXAMASAAA\n" +
-            "MAMMMXMMMM\n" +
-            "MXMXAXMASX";
+    public static String code = "";
 
     public static void main(String[] args) {
         int matches = 0;
@@ -37,18 +27,11 @@ public class Day4B {
                 String tLLetter = String.valueOf(getLetter(x-1, y+1, lineWidth));
                 String tRLetter = String.valueOf(getLetter(x+1, y+1, lineWidth));
                 String bLLetter = String.valueOf(getLetter(x-1, y-1, lineWidth));
-                String bRLetter = String.valueOf(getLetter(x+1, y+1, lineWidth));
+                String bRLetter = String.valueOf(getLetter(x+1, y-1, lineWidth));
 
-                System.out.println(tLLetter + ". " + tRLetter);
-                System.out.println(". " + letter + ". ");
-                System.out.println(bLLetter + ". " + bRLetter);
-
-                System.out.println("bRLetter: " + bRLetter + ", bLLetter: " + bLLetter);
-                System.out.println(tLLetter +" == " + opposites.get(bRLetter) + " && " + tRLetter + " == " + opposites.get(bLLetter));
                 if (tLLetter.equals(opposites.get(bRLetter)) && tRLetter.equals(opposites.get(bLLetter))) {
                     matches++;
                 }
-                System.out.println("");
 
                 j++;
             }
